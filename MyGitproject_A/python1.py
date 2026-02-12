b38e2ac50e043fdd3cc23ee77f83ebd45d72ad21
Pankaj ubale <pankajsubale14@gmail.com>
2026-02-12 20:25:20 +0530

    week 6:Scaling Considerations(with Redis Pub/Sub)

--- MyGitproject_A/python1.py
+++ MyGitproject_A/python1.py
@@ -1,66 +1,72 @@
-import uvicorn
+import asyncio
+import json
+import redis.asyncio as redis
+from contextlib import asynccontextmanager
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 from pydantic import BaseModel
-from typing import List, Dict
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app:FastAPI):
+    task = asyncio.create_task(manager.listen_for_broadcasts())
+    yield
+    task.cancel() #stoping from task shutdown
 
-#  data model
-class NotificationSchema(BaseModel):
-    message:str
-    room_name:str
+app = FastAPI(lifespan=lifespan)
 
 # The Connection Manager ---
 class ConnectionManager:
     def __init__(self):
-       self.active_connections: Dict[str, List[WebSocket]] = {}
+       self.active_connections: dict = {}
+       self.redis_client=redis.from_url("redis://localhost:6379",decode_responses=True)
+       self.pubsub=self.redis_client.pubsub()
 
-    async def connect(self, websocket: WebSocket, room_name: str):
-        
+    async def connect(self, websocket: WebSocket, room_id: str):
         await websocket.accept()
-        if room_name not in self.active_connections:
-            self.active_connections[room_name] = []
+        if room_id not in self.active_connections:
+            self.active_connections[room_id] = []
             #store in the user room 
-        self.active_connections[room_name].append(websocket)
-        print(f"user joined room : {room_name}")
+        self.active_connections[room_id].append(websocket)
     
-    def disconnect(self,websocket:WebSocket,room_name:str):
-        if room_name in self.active_connections:
-            self.active_connections[room_name].remove(websocket)
+    def disconnect(self,websocket:WebSocket,room_id:str):
+        if room_id in self.active_connections:
+            self.active_connections[room_id].remove(websocket)
 
-    async def relay_notification(self,message:str,room_name:str):
-        if room_name in self.active_connections:
-            for connection in self.active_connections[room_name]:
-                await connection.send_text(message)
+    async def listen_for_broadcasts(self):
+        #background task for hears messages
+        await self.pubsub.subscribe("internship_notification")
+        async for message in self.pubsub.listen():
+            if message["type"]=="message":
+                data = json.loads(message["data"])
+                #send the message to local client
+                await self.broadcasts_locally(data["room_id"],data["message"])
 
-    async def broadcast_chat(self,message:str,room_name:str,sender:WebSocket):
-        if room_name in self.active_connections:
-            for connection in self.active_connections[room_name]:
-                if connection != sender:
+        async def broadcasts_locally(self,room_id:str,message:str):
+            #send the message only to people connected to this specific server.
+            if room_id in self.active_connections:
+                for connection in self.active_connections[room_id]:
                     await connection.send_text(message)
 
 manager = ConnectionManager()
 #HTTP  ENDPOINT
-@app.post("/api/notify")
-async def send_notification(notification:NotificationSchema):
-    print(f"Received API request for room : {notification.room_name}")
+class Notification(BaseModel):
+    room_id:str
+    message:str
 
-    await manager.relay_notification(notification.message,notification.room_name)
-
-    return {"status": "notification send"}
+@app.post("/notify/")
+async def trigger_notification(notification:Notification):
+    payload = json.dumps({
+        "room_id":notification.room_id,
+        "message":notification.message
+        })
+    await manager.redis_client.publish("internship_notifications",payload)
+    return {"status": "Notification sent to Redis bus"}
 
 #The WebSocket Endpoint ---
-@app.websocket("/ws/{room_name}/{user_id}")
-async def websocket_endpoint(websocket: WebSocket, room_name: str, user_id: str):
-    await manager.connect(websocket, room_name)
+@app.websocket("/ws/{room_id}")
+async def websocket_endpoint(websocket: WebSocket, room_id: str, user_id: str):
+    await manager.connect(websocket, room_id)
     try:
-        await manager.relay_notification(f"system:{user_id}joined the chat.",room_name)
         while True:
             await websocket.receive_text()
-
     except WebSocketDisconnect:
-        manager.disconnect(websocket, room_name)
-        await manager.relay_notification(f"system:{user_id} left.",room_name)
-#server start
-if __name__=="__main__":
-    uvicorn.run(app, host="0.0.0.0",port=8000)
+        manager.disconnect(websocket, room_id)
